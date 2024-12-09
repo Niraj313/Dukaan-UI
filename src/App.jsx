9343c67b52476d4ecd1cea519328a6330694b326
Niraj Gupta <coder4117@gmail.com>
2024-12-09 16:03:55 +0530

    Navbar Done

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import './App.css'
 import RevenueCard from './components/RevenueCard'
+import Topbar from './components/Topbar'
 
 function App() {
   return (
     <>
+      <Topbar />
       <RevenueCard />
     </>
   )
